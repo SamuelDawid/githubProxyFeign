b33d486c6029aa8b0f4b84cd58f7d0221931a453
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-01 14:33:21 +0200

    feat: Created GiHubClient and his Response Dto

---
+++ src/main/java/com/githubProxy/gitHubClient/GitHubClient.java
@@ -0,0 +1,11 @@
+package com.githubProxy.gitHubClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "githubClient", url = "$.{app.githubClient.url",configuration = {})
+public interface GitHubClient {
+    @GetMapping("/{owner}/{repository-name}")
+     GitHubResponse getByOwnerAndRepositoryName(@PathVariable("owner") String owner,@PathVariable("repository-name") String repositoryName);
+}
