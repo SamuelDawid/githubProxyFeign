7926953ab4f2d0572545c199c26eefb7318677f7
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-02 14:33:22 +0200

    feat: created RepositoryEntity

---
+++ src/main/java/com/githubProxy/models/GitHubRepositoryEntity.java
@@ -0,0 +1,25 @@
+package com.githubProxy.models;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "GIT_HUB_REPOSITORY", uniqueConstraints = @UniqueConstraint(columnNames = {"owner", "repository_name"}))
+public class GitHubRepositoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String owner;
+    private String repositoryName;
+    private String fullName;
+    private String description;
+    private String cloneUrl;
+    private Long stargazersCount;
+    private OffsetDateTime createdAt;
+}
