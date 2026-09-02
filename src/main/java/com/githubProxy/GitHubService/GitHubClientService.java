45dd8d6ab91816c1d6e866d186b6ad4f05e96720
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-02 14:45:23 +0200

    feat: create method in service

--- src/main/java/com/githubProxy/GitHubService/GitHubClientService.java
+++ src/main/java/com/githubProxy/GitHubService/GitHubClientService.java
@@ -1,9 +1,11 @@
 package com.githubProxy.GitHubService;
 
 import com.githubProxy.dto.RepositoryDto;
+import com.githubProxy.dto.gitHubRepositoryEntity.GitHubRepositoryDto;
 import com.githubProxy.gitHubClient.GitHubClient;
 import com.githubProxy.gitHubClient.GitHubResponse;
 import com.githubProxy.mappers.GitHubClientMapper;
+import com.githubProxy.models.GitHubRepositoryEntity;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,5 +23,13 @@ public class GitHubClientService {
         GitHubResponse response = gitHubClient.getByOwnerAndRepositoryName(owner, repositoryName);
         log.info("Repository {} found with this details : {}",repositoryName,response);
         return gitHubClientMapper.toDto(response);
+    }
+    public GitHubRepositoryDto create(@NonNull String owner, @NonNull String repositoryName){
+       log.info("Creating GitHubRepository entity for owner: {}, repository name: {}",owner,repositoryName);
+        GitHubResponse response = gitHubClient.getByOwnerAndRepositoryName(owner, repositoryName);
+        log.info("Found github repository {}",response);
+        GitHubRepositoryEntity entity = gitHubClientMapper.toEntity(response,owner,repositoryName);
+        log.info("Successfully Created Repository entity {}", entity);
+        return gitHubClientMapper.toRepositoryDto(entity);
     }
 }
