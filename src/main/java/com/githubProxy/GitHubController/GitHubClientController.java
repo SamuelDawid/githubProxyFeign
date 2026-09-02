5f11e02fb7028f170b5abd85b83d136d68bb0a6e
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-02 14:45:43 +0200

    feat: added post mapping to GitHubClientController

--- src/main/java/com/githubProxy/GitHubController/GitHubClientController.java
+++ src/main/java/com/githubProxy/GitHubController/GitHubClientController.java
@@ -2,11 +2,10 @@ package com.githubProxy.GitHubController;
 
 import com.githubProxy.GitHubService.GitHubClientService;
 import com.githubProxy.dto.RepositoryDto;
+import com.githubProxy.dto.gitHubRepositoryEntity.GitHubRepositoryDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +16,11 @@ public class GitHubClientController {
     @GetMapping("/{owner}/{repository-name}")
     public RepositoryDto getByOwnerAndRepositoryName(@PathVariable("owner") String owner, @PathVariable("repository-name") String repositoryName){
        return service.getByOwnerAndRepositoryName(owner,repositoryName);
+    }
+
+    @PostMapping("/{owner}/{repository-name}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public GitHubRepositoryDto create(@PathVariable("owner") String owner, @PathVariable("repository-name") String repositoryName){
+        return service.create(owner,repositoryName);
     }
 }
