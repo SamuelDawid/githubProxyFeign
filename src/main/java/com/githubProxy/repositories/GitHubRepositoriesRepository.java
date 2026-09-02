2ddf214f637aa895fcdbed44314a4efccfa658d2
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-02 14:33:46 +0200

    feat: created Repository for gitHubrepositoryentity

---
+++ src/main/java/com/githubProxy/repositories/GitHubRepositoriesRepository.java
@@ -0,0 +1,12 @@
+package com.githubProxy.repositories;
+
+import com.githubProxy.dto.RepositoryDto;
+import com.githubProxy.models.GitHubRepositoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GitHubRepositoriesRepository extends JpaRepository<GitHubRepositoryEntity, Long> {
+    List<RepositoryDto> findAllByOwner(String owner);
+    RepositoryDto findByOwnerAndRepositoryName(String owner,String repositoryName);
+}
