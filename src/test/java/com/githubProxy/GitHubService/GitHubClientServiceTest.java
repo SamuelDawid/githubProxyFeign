95826332ada2242685dd833e68956ba49704a632
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-03 15:06:40 +0200

    feat: added test for update method

--- src/test/java/com/githubProxy/GitHubService/GitHubClientServiceTest.java
+++ src/test/java/com/githubProxy/GitHubService/GitHubClientServiceTest.java
@@ -1,5 +1,6 @@
 package com.githubProxy.GitHubService;
 
+import com.githubProxy.dto.GitHubRepositoryPutCommand;
 import com.githubProxy.dto.RepositoryDto;
 import com.githubProxy.dto.gitHubRepositoryEntity.GitHubRepositoryDto;
 import com.githubProxy.exceptions.LocalRepositoryNotFoundException;
@@ -10,7 +11,6 @@ import com.githubProxy.gitHubClient.GitHubResponse;
 import com.githubProxy.mappers.GitHubClientMapper;
 import com.githubProxy.models.GitHubRepositoryEntity;
 import com.githubProxy.repositories.GitHubRepositoriesRepository;
-import jakarta.persistence.Column;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -104,26 +104,27 @@ class GitHubClientServiceTest {
         );
         when(repository.findByOwnerAndRepositoryName(owner, repoName)).thenReturn(Optional.of(returned));
         //When
-        GitHubRepositoryDto result = service.getLocalRepositoryByOwnerAndRepositoryName(owner,repoName);
+        GitHubRepositoryDto result = service.getLocalRepositoryByOwnerAndRepositoryName(owner, repoName);
         //Then
         assertAll(
-                () -> assertEquals(expected.fullName(),result.fullName()),
-                () -> assertEquals(expected.cloneUrl(),result.cloneUrl()),
-                () -> assertEquals(expected.createdAt(),result.createdAt()),
-                () -> assertEquals(expected.description(),result.description()),
-                () -> assertEquals(expected.stars(),result.stars())
+                () -> assertEquals(expected.fullName(), result.fullName()),
+                () -> assertEquals(expected.cloneUrl(), result.cloneUrl()),
+                () -> assertEquals(expected.createdAt(), result.createdAt()),
+                () -> assertEquals(expected.description(), result.description()),
+                () -> assertEquals(expected.stars(), result.stars())
         );
     }
+
     @Test
-    void getLocalRepositoryByOwnerAndRepositoryName_WhenLocalRepositoryDoesNotExists_ShouldThrowLocalRepositoryNotFoundException(){
+    void getLocalRepositoryByOwnerAndRepositoryName_WhenLocalRepositoryDoesNotExists_ShouldThrowLocalRepositoryNotFoundException() {
         //Given
         String owner = "owner";
         String repoName = "SomeExistingRepository";
-        when(repository.findByOwnerAndRepositoryName(owner,repoName)).thenReturn(Optional.empty());
+        when(repository.findByOwnerAndRepositoryName(owner, repoName)).thenReturn(Optional.empty());
         //When + Then
         LocalRepositoryNotFoundException exception = assertThrowsExactly(LocalRepositoryNotFoundException.class,
-                () -> service.getLocalRepositoryByOwnerAndRepositoryName(owner,repoName));
-        assertEquals("Local repository " + repoName + " not found for: " + owner,exception.getMessage());
+                () -> service.getLocalRepositoryByOwnerAndRepositoryName(owner, repoName));
+        assertEquals("Local repository " + repoName + " not found for: " + owner, exception.getMessage());
     }
 
 
@@ -140,7 +141,6 @@ class GitHubClientServiceTest {
                 "http://api.github.someUrl",
                 3L,
                 OffsetDateTime.of(LocalDateTime.of(2000, 3, 11, 15, 12), ZoneOffset.UTC)
-
         );
         when(repository.save(any(GitHubRepositoryEntity.class))).thenReturn(expected);
         //When
@@ -196,9 +196,45 @@ class GitHubClientServiceTest {
         verify(repository, never()).save(any());
     }
 
+    @Test
+    void update_WhenRepositoryExists_ShouldUpdateAndReturnMatchingDto() {
+        //Given
+        String owner = "Owner";
+        String repoName = "AlreadySavedRepository";
+        GitHubRepositoryEntity existing = buildRepositoryEntity(
+                1L,
+                owner,
+                repoName,
+                "Owner/ExistingRepo",
+                "Some description",
+                "http://api.github.someUrl",
+                3L,
+                OffsetDateTime.of(LocalDateTime.of(2000, 3, 11, 15, 12), ZoneOffset.UTC)
+        );
+        GitHubRepositoryPutCommand putCommand = new GitHubRepositoryPutCommand(
+                null,
+                "Some new fascinating description",
+                null,
+                10L,
+                null
+        );
+        when(repository.findByOwnerAndRepositoryName(owner, repoName)).thenReturn(Optional.of(existing));
+        //When
+        GitHubRepositoryDto result = service.update(owner, repoName, putCommand);
+        //Then
+        assertAll(
+                () -> assertEquals("Owner/ExistingRepo", result.fullName()),
+                () -> assertEquals("Some new fascinating description", result.description()),
+                () -> assertEquals("http://api.github.someUrl", result.cloneUrl()),
+                () -> assertEquals(10L, result.stars()),
+                () -> assertEquals(OffsetDateTime.of(LocalDateTime.of(2000, 3, 11, 15, 12), ZoneOffset.UTC), result.createdAt())
+        );
+        verify(repository).save(any(GitHubRepositoryEntity.class));
+    }
+
     private GitHubRepositoryEntity buildRepositoryEntity(Long id, String owner, String repositoryName, String fullName, String description, String cloneUrl, Long stargazersCount, OffsetDateTime createdAt) {
         GitHubRepositoryEntity entity = new GitHubRepositoryEntity(owner, repositoryName, fullName, description, cloneUrl, stargazersCount, createdAt);
-        ReflectionTestUtils.setField(entity,"id",id);
+        ReflectionTestUtils.setField(entity, "id", id);
         return entity;
     }
 }
