7e7374934df8a33005751716e3de12744c5cb9df
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-03 11:59:28 +0200

    fix: renamed

--- src/main/java/com/githubProxy/exceptions/RepositoryAlreadyExistsException.java
+++ src/main/java/com/githubProxy/exceptions/RepositoryAlreadyExistsException.java
@@ -3,8 +3,8 @@ package com.githubProxy.exceptions;
 import com.githubProxy.exceptions.handler.GitHubClientException;
 import org.springframework.http.HttpStatus;
 
-public class RepositoryAlreadyExists extends GitHubClientException {
-    public RepositoryAlreadyExists(String owner,String repositoryName) {
+public class RepositoryAlreadyExistsException extends GitHubClientException {
+    public RepositoryAlreadyExistsException(String owner,String repositoryName) {
 
         super(owner + " already has repository " + repositoryName, HttpStatus.CONFLICT);
     }
