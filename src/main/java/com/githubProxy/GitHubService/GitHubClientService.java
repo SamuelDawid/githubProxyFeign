82147e703eceac59090be28f3be67c28a27e9067
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-03 14:34:39 +0200

    feat: added logic for put method

--- src/main/java/com/githubProxy/GitHubService/GitHubClientService.java
+++ src/main/java/com/githubProxy/GitHubService/GitHubClientService.java
@@ -1,5 +1,6 @@
 package com.githubProxy.GitHubService;
 
+import com.githubProxy.dto.GitHubRepositoryPutCommand;
 import com.githubProxy.dto.RepositoryDto;
 import com.githubProxy.dto.gitHubRepositoryEntity.GitHubRepositoryDto;
 import com.githubProxy.exceptions.LocalRepositoryNotFoundException;
@@ -12,8 +13,8 @@ import com.githubProxy.repositories.GitHubRepositoriesRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,17 +23,19 @@ public class GitHubClientService {
     private final GitHubClient gitHubClient;
     private final GitHubClientMapper gitHubClientMapper;
     private final GitHubRepositoriesRepository repository;
-
+    @Transactional(readOnly = true)
     public RepositoryDto getByOwnerAndRepositoryName(@NonNull String owner, @NonNull String repositoryName) {
         log.info("Finding repository with name {} by {}", repositoryName, owner);
         GitHubResponse response = gitHubClient.getByOwnerAndRepositoryName(owner, repositoryName);
         log.info("Repository {} found with this details : {}",repositoryName,response);
         return gitHubClientMapper.toDto(response);
     }
+    @Transactional(readOnly = true)
     public GitHubRepositoryDto getLocalRepositoryByOwnerAndRepositoryName(@NonNull String owner, @NonNull String repositoryName){
         log.info("Finding local repository {} by {}",repositoryName,owner);
-        return gitHubClientMapper.toRepositoryDto(findRepostioryByOwnerAndNameOrThrow(owner,repositoryName));
+        return gitHubClientMapper.toRepositoryDto(findRepositoryByOwnerAndNameOrThrow(owner,repositoryName));
     }
+    @Transactional
     public GitHubRepositoryDto create(@NonNull String owner, @NonNull String repositoryName){
        log.info("Creating GitHubRepository entity for owner: {}, repository name: {}",owner,repositoryName);
        validateRepository(owner,repositoryName);
@@ -43,6 +46,15 @@ public class GitHubClientService {
         log.info("Successfully Created Repository entity {}", saved);
         return gitHubClientMapper.toRepositoryDto(saved);
     }
+    @Transactional
+    public GitHubRepositoryDto update(@NonNull String owner, @NonNull String repositoryName, @NonNull GitHubRepositoryPutCommand putCommand){
+        log.info("Stared updating local repository {} by {}",owner,repositoryName);
+        GitHubRepositoryEntity entity = findRepositoryByOwnerAndNameOrThrow(owner,repositoryName);
+        entity.updateLocalRepository(putCommand);
+        GitHubRepositoryEntity saved = repository.save(entity);
+        log.info("Successfully updated repository {} -> {}",repositoryName,saved);
+        return gitHubClientMapper.toRepositoryDto(entity);
+    }
 
     private void validateRepository(String owner, String repositoryName){
         if(repository.existsByOwnerAndRepositoryName(owner,repositoryName)){
@@ -50,7 +62,7 @@ public class GitHubClientService {
             throw new RepositoryAlreadyExistsException(owner,repositoryName);
         }
     }
-    private GitHubRepositoryEntity findRepostioryByOwnerAndNameOrThrow(String owner, String repositoryName){
+    private GitHubRepositoryEntity findRepositoryByOwnerAndNameOrThrow(String owner, String repositoryName){
         return repository.findByOwnerAndRepositoryName(owner,repositoryName).orElseThrow(() ->{
                     log.error("Local repository {} not found for {}",repositoryName,owner);
                     return new LocalRepositoryNotFoundException(owner,repositoryName);
